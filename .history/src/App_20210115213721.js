b0a6b641431f0f083a4819254772760625f78e3e
Aqeel Nasrullah <aqeelnasrulah1997@gmail.com>
2021-01-15 21:39:27 +0500

    Configured Appilication

--- .history/src/App_20210115213721.js
+++ .history/src/App_20210115213721.js
@@ -1,10 +1,11 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className="text-center">Good to go</h1>
+        <h1>Good to go</h1>
       </div>
     </div>
   );
--- .history/src/index_20210115213442.js
+++ .history/src/index_20210115213442.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 ReactDOM.render(
   <React.StrictMode>
